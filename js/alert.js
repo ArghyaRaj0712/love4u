6d951437579b6a4527682c7df27c0f2f19dded36
ArghyaRaj Niloy <82107392+ArghyaRaj0712@users.noreply.github.com>
2021-04-28 23:38:02 +0600

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,8 @@
+// error Button
+function btn(){
+    swal("Working On Process!", "Try Again After Some Times!", "error");
+}
+
+function button(){
+  swal("Submitted!", "Thank You!", "success");
+}
